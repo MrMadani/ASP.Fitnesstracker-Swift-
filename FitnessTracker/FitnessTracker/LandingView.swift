9607fdb077e8e6ba19796ba7c2c8f7463a8e30a0
MrMadani <shahinmadani.sm@gmail.com>
2023-02-10 13:42:30 -0500

    second page
    navigation view is used

--- FitnessTracker/FitnessTracker/LandingView.swift
+++ FitnessTracker/FitnessTracker/LandingView.swift
@@ -8,42 +8,50 @@
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive = false
     var body: some View {
-        //GeomatryReader Will give the hight of the container
-        GeometryReader{ proxy in
-            VStack {
-                Spacer().frame(height:proxy.size.height*0.40)
-                Text("FitTracker")
-                    .font(.system(size: 65,
-                                  weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action:{}){
-                    HStack(spacing: 15){
-                        Spacer()
-                        Image(systemName: "plus.circle").font(.system(size:25,weight: .semibold))
-                            .foregroundColor(.white)
-                           
-                        Text("Create a Challenge").font(.system(size:24, weight:.semibold))
-                            .foregroundColor(.white)
-                        Spacer()
+        NavigationView{
+            //GeomatryReader Will give the hight of the container
+            GeometryReader{ proxy in
+                VStack {
+                    Spacer().frame(height:proxy.size.height*0.40)
+                    Text("FitTracker")
+                        .font(.system(size: 65,
+                                      weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: Text("navigated"),isActive: $isActive){
+                        Button(action:{
+                            isActive=true
+                            
+                        }){
+                            HStack(spacing: 15){
+                                Spacer()
+                                Image(systemName:"plus.circle")
+                                    .font(.system(size:25,weight: .semibold))
+                                    .foregroundColor(.white)
+                                
+                                Text("Create a Challenge")
+                                    .font(.system(size:24, weight:.semibold))
+                                    .foregroundColor(.white)
+                                Spacer()
+                            }
+                            
+                        }.padding(.horizontal,15)
+                            .padding(.bottom,35)
+                            .buttonStyle(PrimaryButtonStyle())
                     }
-                    
-                }.padding(.horizontal,15)
-                    .padding(.vertical,15)
-                .buttonStyle(PrimaryButtonStyle())
-
-            }.frame(
-                maxWidth: .infinity,
-                    maxHeight: .infinity)
-            .background(Image("coverPhoto")
-                .resizable().aspectRatio(
-                contentMode:.fill).overlay(Color.black.opacity(0.4))
-                        
-            )
-            .frame(width: proxy.size.width)
-            .edgesIgnoringSafeArea(.all)
-        }
+                }.frame(maxWidth: .infinity,maxHeight: .infinity)
+                //We used the cover picture in the Vstack
+                    .background(Image("coverPhoto")
+                        .resizable().aspectRatio(
+                            contentMode:.fill).overlay(Color.black.opacity(0.4))
+                                
+                    )
+                    .frame(width: proxy.size.width)
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }.accentColor(.primary)
     }
 }
 
